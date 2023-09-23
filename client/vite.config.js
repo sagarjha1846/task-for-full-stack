f452e8d4de6ae5502782c89fbcb0987004c07a94
Sagar Jha <sagarj.ext@shapoorji.com>
2023-09-24 01:12:44 +0530

    changes vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 import { createFilter } from '@rollup/pluginutils';
-import * as XLSX from "xlsx"
+import * as XLSX from 'xlsx';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   output: {
     // other output options...
     build: {
+      chunkSizeWarningLimit: 100000000,
       rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          nested: resolve(__dirname, 'nested/index.html'),
+        },
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
@@ -31,22 +37,18 @@ export default defineConfig({
       }
     },
   },
-  plugins: [
-    react(),
-    {
-      name: 'xlsx-plugin',
-      transform(code, id) {
-        const filter = createFilter(['**/*.xlsx']);
+  name: 'xlsx-plugin',
+  transform(code, id) {
+    const filter = createFilter(['**/*.xlsx']);
 
-        if (filter(id)) {
-          const workbook = XLSX.readFile(id);
-          const sheetName = workbook.SheetNames[0];
-          const worksheet = workbook.Sheets[sheetName];
-          const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-          const transformedCode = `export default ${JSON.stringify(jsonData)};`;
-          return transformedCode;
-        }
-      },
-    },
-  ],
+    if (filter(id)) {
+      const workbook = XLSX.readFile(id);
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+      const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+      const transformedCode = `export default ${JSON.stringify(jsonData)};`;
+      return transformedCode;
+    }
+  },
+  plugins: [react(), splitVendorChunkPlugin()],
 });
