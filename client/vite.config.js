d29d94beb8c29478d0390d02d200a87c0cadcf77
Sagar Jha <sagarj.ext@shapoorji.com>
2023-09-24 01:26:57 +0530

    testing

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   output: {
+    manualChunks(id) {
+      // Specify how modules should be grouped into chunks
+      if (id.includes('node_modules')) {
+        return 'vendor'; // Group all modules from node_modules into a 'vendor' chunk
+      } else {
+        return 'main'; // Group all other modules into a 'main' chunk
+      }
+    },
     // other output options...
     build: {
       chunkSizeWarningLimit: 50000000, // Adjust the chunk size warning limit to 500 kBs
